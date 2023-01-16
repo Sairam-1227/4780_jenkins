4e7c1353a3570b99a156ae55abf1b1eb107ab95d
Gatla Sairam Reddy <122012024+Sairam-1227@users.noreply.github.com>
2023-01-16 15:50:39 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,67 +1,7 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "squares of numbers are...\n",
-      "4\n",
-      "16\n",
-      "36\n",
-      "64\n",
-      "100\n",
-      "cubes of numbers are...\n",
-      "8\n",
-      "64\n",
-      "216\n",
-      "512\n",
-      "1000\n"
-     ]
-    }
-   ],
-   "source": [
-    "numbers = [2,4,6,8,10]\n",
-    "print('squares of numbers are...')\n",
-    "for i in numbers:\n",
-    "    print(i**2)\n",
-    "print('cubes of numbers are...')\n",
-    "for i in numbers:\n",
-    "    print(i**3)\n",
-    "    "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+numbers = [2,4,6,8,10]
+print('squares of numbers are...')
+for i in numbers:
+    print(i**2)
+print('cubes of numbers are...')
+for i in numbers:
+    print(i**3)
